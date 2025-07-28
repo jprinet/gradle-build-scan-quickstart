e6752c51f969bb11609ac8dc1f7135b5736b8ebe
Jerome Prinet <jprinet@gradle.com>
2025-07-28 09:55:01 +0200

    Add develocity configuration

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,6 +1,10 @@
 plugins {
-    id("com.gradle.enterprise") version "3.16.1"
+    id("com.gradle.develocity") version("4.1")
     id("org.gradle.toolchains.foojay-resolver-convention") version "0.7.0"
 }
 
 rootProject.name = "gradle-build-scan-quickstart"
+
+develocity {
+    server.set("https://ge.solutions-team.gradle.com")
+}
